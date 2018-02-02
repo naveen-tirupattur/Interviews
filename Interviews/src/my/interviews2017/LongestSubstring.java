777e45745919a1daa889e5f004cd9f81a6a24268
naveen-tirupattur <naveen.tirupattur@gmail.com>
2018-02-02 07:03:53 -0800

    Added code to find longest substring without repeating characters

--- Interviews/src/my/interviews2017/LongestSubstring.java
+++ Interviews/src/my/interviews2017/LongestSubstring.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 public class LongestSubstring {
 
 	public static void main(String[] args) {
-		System.out.println(findLongestSubString("XWYZABDEXYZC"));
+		System.out.println(findLongestSubString("AAAB"));
 	}
 	
 	public static int findLongestSubString(String s) {
-		int[] lastSeen = new int[26]; // Use array to store the last seen index of elements
+		int[] lastSeen = new int[128]; // Use array to store the last seen index of elements
 		Arrays.fill(lastSeen, -1); 
 		int maxLength = 0, length = 0, start = 0;
 		for (int i=0;i<s.length();i++) {
-			if (lastSeen[s.charAt(i)-'A'] !=-1) { // Check if you have seen this element before
-				start = lastSeen[s.charAt(i)-'A']; 
+			if (lastSeen[s.charAt(i)] !=-1) { // Check if you have seen this element before
+				start = lastSeen[s.charAt(i)]; 
 				length = i - start; // Length of unique sub array
 			} else {
 				length++; // Update the length by 1 if you have not seen this element before
 			}
 			if (length > maxLength) maxLength = length; // Update the maximum length
-			lastSeen[s.charAt(i)-'A'] = i; // set the last seen index for this element
+			lastSeen[s.charAt(i)] = i; // set the last seen index for this element
 		}
 		return maxLength;
 	}
