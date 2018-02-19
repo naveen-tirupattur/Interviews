dc5a3c19b5c0d4d06746661f852609cdb1f3a8fe
naveen-tirupattur <naveen.tirupattur@gmail.com>
2018-02-19 13:32:04 -0800

    Added code to find max product except self

---
+++ Interviews/src/my/interviews2017/hard/MaxiumProductExceptSelf.java
@@ -0,0 +1,21 @@
+package my.interviews2017.hard;
+
+import my.interviews2017.ProductExceptSelf;
+
+public class MaxiumProductExceptSelf {
+
+	public static void main(String[] args) {
+		int[] a = {3,2,-1,7,-1,6};
+		System.out.println(findMaximumProduct(a));
+	}
+	
+	public static int findMaximumProduct(int[] a) {
+		int[] result = ProductExceptSelf.product(a);
+		int maximumProduct = 0;
+		for (int i=0;i<result.length;i++) {
+			if (result[i] >= maximumProduct) maximumProduct = result[i];
+		}
+		return maximumProduct;
+	}
+
+}
