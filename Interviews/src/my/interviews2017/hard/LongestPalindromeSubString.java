edcd0a7c56bed564783b3c08c811071e8d2ccd3f
naveen-tirupattur <naveen.tirupattur@gmail.com>
2018-02-20 11:29:11 -0800

    Modified code to print the longest string

--- Interviews/src/my/interviews2017/hard/LongestPalindromeSubString.java
+++ Interviews/src/my/interviews2017/hard/LongestPalindromeSubString.java
@@ -8,38 +8,38 @@ public class LongestPalindromeSubString {
 		System.out.println(longest("forgeeksskeegfor"));
 	}
 	
-	public static int longest(String s) {
+	public static String longest(String s) {
 		int maxLength = 1;
-		String maxString = "";
-		int start, end = 0;
-		
-		for (int i=1;i<s.length();i++) {	
-			//For even length substrings
-			start = i-1;
-			end = i;
-			while (start>=0 && end < s.length() && s.charAt(end) == s.charAt(start)) { // Keep going while the current string between start, end is a palindrome
-				start--;
-				end++;
-			}
-			if (end-start-1 >= maxLength) { // Not a palindrome anymore, check the maximum length
-				maxLength = end-start-1;
-				maxString = s.substring(start+1, end);
-			}
-			
-			
-			// For odd length substrings
-			start = i-1;
-			end = i+1;
-			while (start>=0 && end < s.length() && s.charAt(end) == s.charAt(start)) {  // Keep going while the current string between start, end is a palindrome
-				start--;
-				end++;
-			}
-			if (end-start-1 >= maxLength) { // Not a palindrome anymore, check the maximum length
-				maxLength = end-start-1;
-				maxString = s.substring(start+1, end);
-			}
-		}
-		System.out.println(maxString);
-		return maxLength;
+	  int start=0, end = 0;
+    int startIndex = 0;
+	  if (s == null || s.isEmpty()) return "";
+    for (int i=1;i<s.length();i++) {	
+      //For even length substrings
+      start = i-1;
+      end = i;
+      while (start>=0 && end < s.length() && s.charAt(end) == s.charAt(start)) { // Keep going while the current string between start, end is a palindrome
+        if (end-start+1 > maxLength) { // Not a palindrome anymore, check the maximum length
+          maxLength = end-start+1;
+          startIndex = start;
+        }
+        start--;
+        end++;
+      }
+      
+
+
+      // For odd length substrings
+      start = i-1;
+      end = i+1;
+      while (start>=0 && end < s.length() && s.charAt(end) == s.charAt(start)) {  // Keep going while the current string between start, end is a palindrome
+        if (end-start+1 > maxLength) { // Not a palindrome anymore, check the maximum length
+          maxLength = end-start+1;
+          startIndex = start;
+        }
+        start--;
+        end++;
+      }
+    }
+    return s.substring(startIndex, startIndex+maxLength);
 	}
 }
