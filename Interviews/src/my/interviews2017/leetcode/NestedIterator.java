7c8a2396ca95d530e7c11572e7292185f69ad62a
naveen-tirupattur <naveen.tirupattur@gmail.com>
2018-03-01 11:04:38 -0800

    Added code for NestedInteger Iterator

---
+++ Interviews/src/my/interviews2017/leetcode/NestedIterator.java
@@ -0,0 +1,48 @@
+package my.interviews2017.leetcode;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+
+public class NestedIterator implements Iterator<Integer> {
+
+	Stack<NestedInteger> stack = new Stack<NestedInteger>();
+
+	public NestedIterator(List<NestedInteger> nestedList) {
+		if(nestedList==null)
+			return;
+
+		for(int i=nestedList.size()-1; i>=0; i--){
+			stack.push(nestedList.get(i));
+		}
+	}
+
+	@Override
+	public Integer next() {
+		return stack.pop().getInteger();
+	}
+
+	@Override
+	public boolean hasNext() {
+		while(!stack.isEmpty()){
+			NestedInteger top = stack.peek();
+			if(top.isInteger()){
+				return true;
+			}else{
+				stack.pop();
+				for(int i=top.getList().size()-1; i>=0; i--){
+					stack.push(top.getList().get(i));
+				}
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+}
